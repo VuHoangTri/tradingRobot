35a7cbef11ef50d27d4d59aa9dc05e9c33d76460
TriVu95 <62229814+TriVu95@users.noreply.github.com>
2023-04-24 08:35:45 +0700

    Update slack.ts

--- slack.ts
+++ slack.ts
@@ -12,7 +12,7 @@ export async function sendChatToBot(icon: string, text: string, botNumber: numbe
     const urlBot = 'https://hooks.slack.com/services/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';
     const bot = ['https://hooks.slack.com/services/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',
  'https://hooks.slack.com/services/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',
- 'https://hooks.slack.com/services/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']
+ 'https://hooks.slack.com/services/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'];
     const body =
     {
         "blocks": [
@@ -31,6 +31,30 @@ export async function sendChatToBot(icon: string, text: string, botNumber: numbe
         ]
     };
     await fetch(bot[botNumber],
+        {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' }
+        })
+}
+
+export async function sendError(text: string) {
+   
+    const urlBot = 'https://hooks.slack.com/services/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';
+    const errBot = 'https://hooks.slack.com/services/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';
+    const body =
+    {
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": text
+                }
+            }
+        ]
+    };
+    await fetch(errBot,
         {
             method: 'POST',
             body: JSON.stringify(body),
