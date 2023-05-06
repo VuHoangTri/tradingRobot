ac3da87ef0f9005c4194a3722072ac5b470637b2
VuHoangTri <vuhoangtri@gmail.com>
2023-05-07 06:34:24 +0700

    Change mexc trader of account 2

--- constant.ts
+++ constant.ts
@@ -44,8 +44,8 @@ export const accounts: Account[] = [
         key: 'QXvZHULCw7Lzjw5eqB',
         secret: '7EZOTNFfLO64tJhNiMt3AzSC64qv2H19ftH1',
         testnet: true,
-        trader: "https://futures.mexc.com/copyFutures/api/v1/trader/orders?limit=10&orderListType=CONTRACT&uid=83129161", //08527742"
-        gain: 2020,
+        trader: "https://futures.mexc.com/copyFutures/api/v1/trader/orders?limit=10&orderListType=CONTRACT&uid=69843970", //08527742"
+        gain: 6,
         platform: "Mexc",
         botChat: "https://hooks.slack.com/services/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",
         index: 1
