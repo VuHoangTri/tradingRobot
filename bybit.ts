412bac2df72c55a2699717b78d094a661955addb
TriVu95 <tri_vu@colpal.com>
2023-05-09 10:01:30 +0700

    Show result batch

--- bybit.ts
+++ bybit.ts
@@ -154,9 +154,9 @@ export class BybitAPI {
                     const chunkBatchOrders: BatchOrders = _.cloneDeep(batchOrders);
                     chunkBatchOrders.request = chunkBatchOrders.request.slice(i, i + 9);
                     const resCreate: any = await this.submitBatchOrders(chunkBatchOrders);
+                    console.log(resCreate, resCreate.result.list, resCreate.retExtInfo.list);
                     if (resCreate) {
                         if (resCreate.retCode === 0) {
-                            console.log(resCreate.retExtinfo.list);
                             for (let i = 0; i < resCreate.result.list.length; i++) {
                                 if (resCreate.result.list[i].orderId !== '') {
                                     const order = batchOrders.request[i];
